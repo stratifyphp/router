1639b32b0b13289a9037b70a5192c69cb6e86401
Matthieu Napoli <matthieu@mnapoli.fr>
2015-09-01 17:51:01 +0200

    Add a PrefixRouter that matches path prefixes to the URI

---
+++ src/PrefixRouter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Stratify\Router;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Stratify\Http\Middleware\Invoker\MiddlewareInvoker;
+use Stratify\Http\Middleware\Invoker\SimpleInvoker;
+
+/**
+ * Routes requests by matching prefixes on the URI.
+ *
+ * @author Matthieu Napoli <matthieu@mnapoli.fr>
+ */
+class PrefixRouter
+{
+    /**
+     * @var array[]
+     */
+    private $routes;
+
+    /**
+     * @var MiddlewareInvoker
+     */
+    private $invoker;
+
+    public function __construct(array $routes, MiddlewareInvoker $invoker = null)
+    {
+        $this->invoker = $invoker ?: new SimpleInvoker();
+        $this->routes = $routes;
+    }
+
+    public function __invoke(
+        ServerRequestInterface $request,
+        ResponseInterface $response,
+        callable $next
+    ) : ResponseInterface
+    {
+        $path = $request->getUri()->getPath();
+
+        foreach ($this->routes as $pathPrefix => $middleware) {
+            if (substr($path, 0, strlen($pathPrefix)) === $pathPrefix) {
+                return $this->invoker->invoke($middleware, $request, $response, $next);
+            }
+        }
+
+        return $next($request, $response);
+    }
+}
---
+++ tests/PrefixRouterTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Stratify\Router\Test;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Stratify\Http\Middleware\Invoker\MiddlewareInvoker;
+use Stratify\Router\PrefixRouter;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequest;
+
+class PrefixRouterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function routes_request_based_on_path_prefix()
+    {
+        $router = new PrefixRouter([
+            '/api'    => function (ServerRequestInterface $request, ResponseInterface $response) {
+                $response->getBody()->write('API');
+                return $response;
+            },
+            '/admin' => function (ServerRequestInterface $request, ResponseInterface $response) {
+                $response->getBody()->write('Admin');
+                return $response;
+            },
+        ]);
+
+        $response = $router->__invoke($this->request('/api/test'), new Response, $this->next());
+        $this->assertEquals('API', $response->getBody()->__toString());
+        $response = $router->__invoke($this->request('/admin'), new Response, $this->next());
+        $this->assertEquals('Admin', $response->getBody()->__toString());
+    }
+
+    /**
+     * @test
+     */
+    public function calls_next_middleware_if_no_route_matched()
+    {
+        $next = function (ServerRequestInterface $request, ResponseInterface $response) {
+            $response->getBody()->write('Hello world!');
+            return $response;
+        };
+
+        $router = new PrefixRouter([
+            '/api'    => function (ServerRequestInterface $request, ResponseInterface $response) {
+                return $response;
+            },
+        ]);
+
+        $response = $router->__invoke($this->request('/'), new Response, $next);
+        $this->assertEquals('Hello world!', $response->getBody()->__toString());
+    }
+
+    /**
+     * @test
+     */
+    public function invokes_middleware_using_invoker()
+    {
+        $routes = [
+            '/api' => 'controller',
+        ];
+
+        $invoker = $this->getMockForAbstractClass(MiddlewareInvoker::class);
+        // Expect controller is invoked
+        $invoker->expects($this->once())
+            ->method('invoke')
+            ->with('controller')
+            ->willReturn(new Response);
+
+        $router = new PrefixRouter($routes, $invoker);
+        $router->__invoke($this->request('/api/test'), new Response, $this->next());
+    }
+
+    private function request($uri)
+    {
+        return new ServerRequest([], [], $uri, 'GET');
+    }
+
+    private function next()
+    {
+        return function () {
+            throw new \Exception('No route matched');
+        };
+    }
+}
