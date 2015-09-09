551c647eb826256eef668acc285bd0e4fe3bd001
Matthieu Napoli <matthieu@mnapoli.fr>
2015-09-09 17:46:31 +0200

    Fix tests

--- tests/Mock/FakeRestController.php
+++ tests/Mock/FakeRestController.php
@@ -6,29 +6,33 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Zend\Diactoros\Response\HtmlResponse;
 
+/**
+ * We have to use static functions because the "SimpleInvoker" doesn't do auto-instantiation
+ * of controllers.
+ */
 class FakeRestController
 {
-    public function index(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    public static function index(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         return new HtmlResponse('Index');
     }
 
-    public function post(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    public static function post(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         return new HtmlResponse('Post');
     }
 
-    public function get(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    public static function get(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         return new HtmlResponse('GET '.$request->getAttribute('id'));
     }
 
-    public function put(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    public static function put(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         return new HtmlResponse('PUT '.$request->getAttribute('id'));
     }
 
-    public function delete(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    public static function delete(ServerRequestInterface $request, ResponseInterface $response, callable $next)
     {
         return new HtmlResponse('DELETE '.$request->getAttribute('id'));
     }
